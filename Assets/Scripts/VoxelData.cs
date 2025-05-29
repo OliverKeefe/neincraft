241e70dccfc2c9aa02b92a2ef2e13b1516bb1d99
OliverKeefe <oliverkeefe@tutanota.com>
2025-05-29 20:10:36 +0100

    Fix typo VoxelVerticies -> VoxelVertices

--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2f46745685c0c53018a012548519a2b1912a749982fec3f0a2900ebb8b5e95b
-size 1336
+oid sha256:20bf74810ee2be0154293befe66768190717b3cf16fb9f4cae33c412e0bc3b23
+size 1335
