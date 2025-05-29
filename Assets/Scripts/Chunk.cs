30e32dd0d3976a48dac1d50c6698ae05283b6e14
OliverKeefe <oliverkeefe@tutanota.com>
2025-05-29 19:51:46 +0100

    Fixed texture positions

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a13c925d4d624f969b409bad61955380e7a80b34f57313c2098197660c05cdb8
+size 2893
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2f46745685c0c53018a012548519a2b1912a749982fec3f0a2900ebb8b5e95b
+size 1336
