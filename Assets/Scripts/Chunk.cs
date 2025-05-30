58b4985893d4601dc272072fb93d1addab56b139
OliverKeefe <oliverkeefe@tutanota.com>
2025-05-30 18:37:38 +0100

    Add chunk generation logic

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a13c925d4d624f969b409bad61955380e7a80b34f57313c2098197660c05cdb8
-size 2893
+oid sha256:a036dbffa9aa94a3cb658b2bf0dba522f90e232d549f2a7d8a4f91b47dfccab2
+size 5266
