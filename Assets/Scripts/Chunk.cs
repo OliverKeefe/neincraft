8206de3073ad6a723517d07eec0042ce65f52bed
OliverKeefe <oliverkeefe@tutanota.com>
2025-06-02 20:47:27 +0100

    Increase chunk height, add stone and bedrock textures

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a036dbffa9aa94a3cb658b2bf0dba522f90e232d549f2a7d8a4f91b47dfccab2
-size 5266
+oid sha256:c62104f926cffd5a318e4e4d1434f33c9bd2a617cb6dbc8748b30ae369f77c09
+size 5468
