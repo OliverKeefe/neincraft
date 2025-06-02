373bea89d064fa57c76c85020951113c37f06db2
OliverKeefe <oliverkeefe@tutanota.com>
2025-06-02 20:46:52 +0100

    Add Bedrock block type

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa90cfe5e30b2feb95b7d562a3b5a79e03a30e96860ad77bfa7945fd83801dc8
-size 1296
+oid sha256:98702759f2a10e8cecf40deb3036a732e520dc17862621694b860d380f2d4450
+size 1365
